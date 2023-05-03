d6e889f5b5205f8751e0dca3bd831efcd71894f8
Ariozon <8kamal9@gmail.com>
2023-05-03 11:42:27 +0000

    Добавлен файл с функциями по работе с массивами

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-function sum(array) {
-  return array.reduce((acc, curr) => acc + curr, 0);
+// Функция, которая возвращает сумму всех элементов массива
+function sumArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 }
 
-function multiply(array) {
-  return array.reduce((acc, curr) => acc * curr, 1);
+// Функция, которая находит максимальное число в массиве
+function maxArray(array) {
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
-module.exports = {
-  sum,
-  multiply
-};
